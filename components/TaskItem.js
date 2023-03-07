17b095e3f6d15420bc52a18694a1722dfe3af5aa
MrChikip <mrchikip@users.noreply.github.com>
2023-03-07 01:47:12 -0500

    comentarios agregados y diccionario ampliado

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -1,8 +1,11 @@
+// Importar los componentes necesarios de React Native
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
+// Definir el componente taskItem que recibe como argumento un objeto de tarea
 const taskItem = ({ task }) => {
   return (
+    // Vista que contiene los elementos de texto para el título y descripción de la tarea
     <View style={styles.itemContainer}>
       <Text style={styles.itemTitle}>{task.tittle}</Text>
       <Text style={styles.itemTitle}>{task.description}</Text>
@@ -10,16 +13,18 @@ const taskItem = ({ task }) => {
   );
 };
 
+// Definir los estilos para el componente
 const styles = StyleSheet.create({
   itemContainer: {
-    backgroundColor: "#333333",
-    padding: 20,
-    marginVertical: 8,
-    borderRadius: 5,
+    backgroundColor: "#333333", // Color de fondo del contenedor
+    padding: 20, // Espaciado interno de 20 puntos
+    marginVertical: 8, // Espaciado vertical de 8 puntos
+    borderRadius: 5, // Bordes redondeados con un radio de 5 puntos
   },
   itemTitle: {
-    color: "#FFFFFF",
+    color: "#FFFFFF", // Color de texto blanco
   },
 });
 
+// Exportar el componente taskItem
 export default taskItem;
